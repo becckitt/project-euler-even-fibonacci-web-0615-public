7c728cab3bcd1871e9aed6b2e120bb7e705ec348
Becckitt <hello@beccabarton.com>
2015-08-12 06:42:03 -0400

    finished

--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,36 @@
 # Implement your object-oriented solution here!
+
+class EvenFibonacci
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def find_fibonaccis
+    fib_array = []
+    i = 1
+    while i <= @limit
+      first_num = fib_array[i-1] ||= 1 
+      second_num =  fib_array[i] ||= 1  
+      result = first_num + second_num
+      if result > @limit
+        break
+      end
+      fib_array << result
+      i += 1
+    end
+    fib_array
+  end
+
+
+  def find_evens
+    find_fibonaccis.select do |num|
+      if num % 2 == 0 && num < @limit
+        num 
+      end
+    end
+  end
+
+  def sum
+    find_evens.reduce(:+)
+  end
+end
